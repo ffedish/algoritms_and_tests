9deafa93be22df35315c541f92dca56c162cceae
ffedish <daniil-fadeev@bk.ru>
2020-02-24 19:25:55 +0300

    Add merge test

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -18,5 +18,25 @@ namespace UnitTest
             foreach (var i in Program.Prime(8))
                 Assert.AreEqual(2, i);
         }
+        [TestMethod]
+        public void merge_correction()
+        {
+            int[] a, b;
+            a = new int[1];
+            b = new int[1];
+            b[0] = 7;
+            a[0] = 4;
+            var c = Program.TwoArr(b, a);
+            Assert.AreEqual(c[0], 4);
+            Assert.AreEqual(c[1], 7);
+            a = new int[0];
+            b = new int[0];
+            Assert.AreEqual(0, Program.TwoArr(a, b).Length);
+            a = new int[3] { 7, 44, 88};
+            b = new int[5] { 49, 50, 51, 100, 502 };
+            c = Program.TwoArr(b, a);
+            for (int i = 1; i < c.Length; i++)
+                Assert.IsTrue(c[i] > c[i - 1]);
+        }
     }
 }
