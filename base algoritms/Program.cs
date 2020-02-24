02fb56deecd6c5fc2af1c652fe055f539a990892
Denis <den89181827071@mail.ru>
2020-02-24 19:20:01 +0300

    MySolution

--- base algoritms/Program.cs
+++ base algoritms/Program.cs
@@ -10,7 +10,44 @@ namespace Test1
     {
         public static int[] TwoArr(int[] arr1, int[] arr2)
         {
-            return null;
+            int[] arr = new int[arr1.Length + arr2.Length];
+            int k = 0;
+            int t = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (k != arr1.Length && t != arr2.Length)
+                {
+                    if (arr1[k] < arr2[t])
+                    {
+                        arr[i] = arr1[k];
+                        k++;
+                        continue;
+                    }
+                    if (arr1[k] >= arr2[t])
+                    {
+                        arr[i] = arr2[t];
+                        t++;
+                        continue;
+                    }
+
+                }
+                if (k == arr1.Length || t == arr2.Length)
+                {
+                    if (k == arr1.Length)
+                    {
+                        arr[i] = arr2[t];
+                        t++;
+                        continue;
+                    }
+                    if (t == arr2.Length)
+                    {
+                        arr[i] = arr1[k];
+                        k++;
+                        continue;
+                    }
+                }
+            }
+            return arr;
         }
         public static List<int> Prime(int n)
         {
@@ -34,11 +71,13 @@ namespace Test1
         }
         static void Main(string[] args)
         {
-            List<int> list = Prime(Convert.ToInt32(Console.ReadLine()));
-            for(int i = 0; i < list.Count(); i++)
-            {
-                Console.Write(list[i] + " ");
-            }
+            /*  List<int> list = Prime(Convert.ToInt32(Console.ReadLine()));
+              for(int i = 0; i < list.Count(); i++)
+              {
+                  Console.Write(list[i] + " ");
+              } */
+            
+            
         }
     }
 }
