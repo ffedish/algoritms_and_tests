346c6dd0eec96c2e075494f0021b9f3a2cbc0d51
ffedish <daniil-fadeev@bk.ru>
2020-02-24 17:44:31 +0300

    add Soulution

---
+++ base algoritms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace base_algoritms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
