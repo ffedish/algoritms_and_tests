8132f99d1fd0f141f8deb2f5551f82493839aebb
Denis <den89181827071@mail.ru>
2020-02-24 17:50:40 +0300

    First Program

--- base algoritms/Program.cs
+++ base algoritms/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace base_algoritms
+namespace Test1
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<uint> Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            uint n = Convert.ToUInt32(Console.ReadLine());
+            List<uint> listOfPrimal = new List<uint>();
+            for (uint i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    while (n % i == 0)
+                    {
+                        listOfPrimal.Add(i);
+                        n = n / i;
+                    }
+                }
+            }
+            if (n > 1)
+            {
+                listOfPrimal.Add(n);
+            }
+            return listOfPrimal;
         }
     }
 }
+
